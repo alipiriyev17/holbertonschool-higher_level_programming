ec222440b5f9778e36115a7b6c8fc65ee272c851
alipiriyev17 <13563@holbertonstudents.com>
2026-03-12 19:23:56 +0000

    Add 6-print_comb3.py with README

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i < 99:
-        print("{:02d}".format(i), end=", ")
-    else:
-        print("{:02d}".format(i))
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
