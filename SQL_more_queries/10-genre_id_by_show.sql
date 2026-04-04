a13eaac2c7dec0b6369a9cee5cc0260c9be031f9
alipiriyev17 <13563@holbertonstudents.com>
2026-04-04 15:43:15 +0000

    Add 10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,11 @@
--- List all TV shows that have at least one genre linked with genre_id
--- Sort by tv_shows.title and tv_show_genres.genre_id
+-- Task 10: Genre ID by show
+-- List all shows that have at least one genre linked
+-- Display: tv_shows.title - tv_show_genres.genre_id
+-- Results sorted by title and genre_id ascending
+-- Only one SELECT statement is used
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
